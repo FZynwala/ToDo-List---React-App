ce80d50e8aee9d06da45e1ced6bc14d2679fff65
unknown <filipzynwala@gmail.com>
2019-10-19 00:26:05 +0200

    Add toggle button

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,10 @@ class App extends React.Component {
         console.log(this.state);
     };
 
+    /*isDoneUpdate = {
+
+    }*/
+
     render() {
         return (
             <div>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,12 +1,42 @@
+import './TaskItem.css';
 import React from 'react';
 
+const buttonConfig = {
+    done: {
+        color: 'green',
+        text: 'DONE'
+    },
+    pending: {
+        color: 'red',
+        text: 'PENDING'
+    }
+};
+
+let toogle = 'pending';
+
 class TaskItem extends React.Component {
+    state = { isDone: false, color: '', text: '' };
+
+    onClickStatus = (event) => {
+        this.setState({isDone: !this.state.isDone});
+        const { color, text } = buttonConfig[this.state.isDone ? 'done' : 'pending'];
+        this.setState({ color, text});
+        console.log(color);
+        console.log(text);
+        console.log(this.state.isDone ? 'done' : 'pending');
+    }
+
     render() {
         return (
-            <div className="item">
+            <div className="item-box item">
                 <div className="content">
                     <div className="header">{this.props.date_created}</div>
                     {this.props.content}
+                </div>
+                <div className="buttons" >
+                    <button className={`button-status ui inverted ${this.state.color} button`} onClick={this.onClickStatus} >{this.state.text}</button>
+                    <button className="ui icon button" ><i className="black edit outline icon"></i></button>
+                    <button className="ui icon button" ><i className="black trash alternate outline icon"></i></button>
                 </div>
             </div>
         );
