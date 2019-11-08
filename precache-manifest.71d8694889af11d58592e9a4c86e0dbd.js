1bad9c61c003486b9b47ad2b69cc386c2b57b926
unknown <filipzynwala@gmail.com>
2019-11-08 20:17:47 +0100

    Updates

---
+++ precache-manifest.71d8694889af11d58592e9a4c86e0dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecaae6017e26e8c93beaa76f35f3431",
+    "url": "/ToDo-List---React-App/index.html"
+  },
+  {
+    "revision": "3093c6110b02e9b7477d",
+    "url": "/ToDo-List---React-App/static/css/main.7ca183e9.chunk.css"
+  },
+  {
+    "revision": "9f4aa225a76d3024abd0",
+    "url": "/ToDo-List---React-App/static/js/2.8a452900.chunk.js"
+  },
+  {
+    "revision": "3093c6110b02e9b7477d",
+    "url": "/ToDo-List---React-App/static/js/main.0fb2c901.chunk.js"
+  },
+  {
+    "revision": "a0f8f3bdeec8df267958",
+    "url": "/ToDo-List---React-App/static/js/runtime-main.e3bf11e0.js"
+  }
+]);
