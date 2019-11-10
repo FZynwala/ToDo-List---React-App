127d641b566d1f160a13f888f2ce21519fa59794
unknown <filipzynwala@gmail.com>
2019-11-10 20:58:00 +0100

    Add google auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import moment from 'moment';
 import InputBar from './InputBar';
 import TaskList from './TaskList';
 import Header from './Header';
 import FilterMenu from './FilterMenu';
+import LoginPage from './LoginPage';
 
 
 let taskId = null;
 
 
 class App extends React.Component {
-    state = { 
-        tasks: [],
-        tasksToDisplay: []
-    };
+    
 
-    onNewTaskSubmit = (term) => {
-        taskId = taskId + 1;
-
-        const newTask = {
-            id: taskId,
-            content: term,
-            isDone: false,
-            date_created: moment(new Date()).format('lll')
-        };
-
-        let temp = this.state.tasks;
-        temp.push(newTask);
-        this.setState({ tasks: temp, tasksToDisplay: temp });
-    };
-
-    onClickDelete = (id)=> {
-        const oldTasks = this.state.tasks;
-        const newTasks = oldTasks.filter(task => task.id !== id);
-        this.setState({ tasks: newTasks, tasksToDisplay: newTasks });
-    };
-
-    onSubmitUpdate = (id, newContent) => {
-        const tasks = this.state.tasks;
-        tasks.forEach(task => { if(task.id === id) { 
-            task.content = newContent;
-        }} );
-        
-        this.setState({ tasks: tasks }); 
-    };
-
-    isDoneChange = (id) => {
-        const tasks = this.state.tasks;
-        tasks.forEach(task => {
-            if(task.id === id) {task.isDone =!task.isDone};
-        });
-        this.setState({ tasks: tasks });
-    };
-
-    filterTasks = (cat) => {
-        switch(cat) {
-            case 'all':
-                this.setState({ tasksToDisplay: this.state.tasks });
-                break;
-            case 'pending':
-                const tasks = this.state.tasks;
-                const pendingTasks = tasks.filter(task => task.isDone === false);
-                this.setState({ tasksToDisplay: pendingTasks });
-                console.log(this.state);
-                break;
-            case 'done':
-                const temp = this.state.tasks;
-                const doneTasks = temp.filter(task => task.isDone === true);
-                this.setState({ tasksToDisplay: doneTasks });
-                break;
-        };
-    };
+    
 
     render() {
         return (
             <div>
-                <Header />
-                <FilterMenu filterTasks={this.filterTasks} />
-                <InputBar onSubmit={this.onNewTaskSubmit} />
-                <TaskList
-                    tasks={this.state.tasksToDisplay}
-                    onClick={this.onClickDelete}
-                    onSubmit={this.onSubmitUpdate}
-                    isDoneChange={this.isDoneChange}
-                />
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route path="/" exact component={LoginPage} />
+                        <Route path="/list" exact component={TaskList} />
+                    </div>
+                </BrowserRouter>
             </div>
         );
     };
---
+++ src/components/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoogleAuth from './GoogleAuth';
+
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <GoogleAuth />
+            </div>
+        );
+    };
+}
+
+export default LoginPage;
